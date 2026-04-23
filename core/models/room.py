a1c4e2d1e53a42b54e3011a02c384b43c43e941e
kauabarros-24 <martinsbarroskaua@gmail.com>
2026-04-23 18:22:27 -0300

    CHORE: Create methods to socket io

--- core/models/room.py
+++ core/models/room.py
@@ -1,10 +1,12 @@
+from gettext import NullTranslations
+
 from django.db import models
 from core.models import User
 import uuid 
 
 class Room(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- core/socket/room.py
+++ core/socket/room.py
@@ -1,5 +1,8 @@
 import logging
 from channels.consumer import AsyncConsumer, database_sync_to_async
+from regex import P
+from rich.pretty import data
+from core.models import User, Room
 
 
 class RoomConsumer(AsyncConsumer):
@@ -18,7 +21,38 @@ class RoomConsumer(AsyncConsumer):
 
 
     @database_sync_to_async
-    def get_db_connnection(self):
+    def get_or_create_room(self, user_uuid, room_uuid: str | None = None):
+        room, _ = Room.objects.get_or_create(user_uuid=user_uuid, uuid=room_uuid)
+
+        return room
+        
+
+    async def receive(self, event):
         pass
+
+
+    @database_sync_to_async
+    def get_user(self, uuid):
+        if not uuid:
+            raise ValueError("UUID is required to fetch user")
+        
+        try:
+            user = User.objects.get(uuid=uuid)
+            return {
+                "uuid": str(user.uuid),
+                "email": user.email,
+                "name": user.name,
+                "profile_image": "Ainda não há uma imagem de perfil"  # Placeholder, update when profile image is implemented
+            }
+        except User.DoesNotExist:
+            raise ValueError("User with the provided UUID does not exist")
+        
+         
+        
+    async def websocket_disconnect(self, event):
+        pass
+        
+
+
 
     
