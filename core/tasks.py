9c3383264813b9b7e73c94b44b64a009770c6c65
kauabarros-24 <martinsbarroskaua@gmail.com>
2026-05-27 12:33:38 -0300

    CHORE: Added a html template

--- core/tasks.py
+++ core/tasks.py
@@ -9,7 +9,11 @@ from django.core.files.base import ContentFile
 from urllib.request import urlopen
 import mimetypes
 import logging
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from core.utils import generate_code
 from urllib.error import URLError, HTTPError
+
 
 
 logger = logging.getLogger(__name__)
@@ -112,3 +116,29 @@ def chatAI_teste():
     chat_ai = ChatAi()
     instance = chat_ai.ai_response
     return instance
+
+@shared_task(autoretry_for=(requests.exceptions.Timeout, requests.exceptions.ConnectionError),retry_backoff=5, retry_kwargs={"max_retries": 3}, soft_time_limit=60)
+def send_email(email):
+    context = {
+            "code": str(generate_code())
+            
+        }
+
+    html_content = render_to_string(
+            "welcome.html",
+            context
+        )
+        
+    send_email = EmailMultiAlternatives(
+            subject="Confirmação de senha",
+            body="",
+            from_email="martinsbarroskaua85@gmail.com",
+            to=[email]
+        )
+    
+    send_email.attach_alternative(
+        html_content,
+        "text/html"
+        )
+
+    send_email.send()  
--- core/views/password.py
+++ core/views/password.py
@@ -1,3 +1,5 @@
+import stat
+
 from rest_framework.views import APIView
 from core.models import User
 from rest_framework.response import Response
@@ -5,6 +7,7 @@ from rest_framework import status
 from core.utils import generate_code
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from core.tasks import send_email
 
 class ForgetPassword(APIView):
     def post(self, request):
@@ -13,28 +16,10 @@ class ForgetPassword(APIView):
         if not email:
             return Response(data={"message": "User not fought"},status=status.HTTP_400_BAD_REQUEST)
         
-        context = {
-            "code": str(generate_code())
-            
-        }
-
-        html_content = render_to_string(
-            "welcome.html",
-            context
-        )
-        
-        send_email = EmailMultiAlternatives(
-            subject="Confirmação de senha",
-            body="",
-            from_email="martinsbarroskaua85@gmail.com",
-            to=[email]
-        )
-
-        send_email.attach_alternative(
-        html_content,
-        "text/html"
-        )
-
-        send_email.send()    
+        new_email = send_email(email)
+         
 
         return Response(data="message", status=status.HTTP_200_OK)
+    
+    def get(self, request):
+        return Response(data={"message": "Hello world"}, status=status.HTTP_200_OK)
