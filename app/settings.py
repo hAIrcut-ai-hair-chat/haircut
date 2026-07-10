02954102ae07ed83e65fefe3330eb5cf67a42584
kauabarros-24 <martinsbarroskaua@gmail.com>
2026-07-10 13:01:37 -0300

    dajdajad

--- app/settings.py
+++ app/settings.py
@@ -89,13 +89,12 @@ TEMPLATES = [
     },
 ]
 
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv(
-            "DATABASE_URL",
-            "sqlite:///" + str(BASE_DIR / "db.sqlite3")
-        ),
-        conn_max_age=600,
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=0,
+        ssl_require=True
     )
 }
 
